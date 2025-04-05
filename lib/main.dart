779b9ae3d83f607d6fc5fce7bd5ee5777433df46
briankaboyi <brianmkaboyi@gmail.com>
2025-04-05 22:01:58 +0300

    wrapped materialapp widget with multiprovider widget and added AiResponseProvider to providers list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:gemini_app/providers/ai_response_provider.dart';
 import 'package:gemini_app/screens/prompt_page.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
@@ -12,13 +14,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        Provider<AiResponseProvider>(create: (_)=> AiResponseProvider(),)
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home:  PromptPage(),
       ),
-      home:  PromptPage(),
     );
   }
 }
